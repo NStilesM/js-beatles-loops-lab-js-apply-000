5a60397ee5cf146740e93025d79d8bb450b7089f
stupendous-keyboard-4608 <nicholas.mcdonnell@gmail.com>
2020-04-25 22:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import { abs } from 'math.js'
 const playtime = []
 
 function theBeatlesPlay(musicians, instruments) {
-  var i = 0;
   for (i = 0; i < musicians.length; i++){
     playtime.push(musicians[i] + " plays " + instruments[i])
   }
