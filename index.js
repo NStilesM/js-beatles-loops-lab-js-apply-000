16b77cb43cca344806146de38af59beb5c6e0da8
stupendous-keyboard-4608 <nicholas.mcdonnell@gmail.com>
2020-04-25 20:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const playtime = []
 
 function theBeatlesPlay(musicians, instruments) {
   for (i = 0; i < musicians.length; i++){
-    playtime.push(musicians[i] + "plays" + instruments[i])
+    playtime.push(musicians[i] + " plays " + instruments[i])
   }
   return playtime
 }
+
+
+var i = 0;
+function johnLennonFacts(facts){
+  while (i < 10){
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts
+}
+
